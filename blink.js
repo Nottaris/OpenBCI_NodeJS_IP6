8bede43da263d533b18ab0a299932e1aa2ae3e27
Michael Job <budiman.job@gmail.com>
2018-04-03 09:20:23 +0200

    timestamp

--- blink.js
+++ blink.js
@@ -4,8 +4,7 @@
 module.exports = {
     getBlinks: function(sample) {
        /** Work with sample */
-    console.log(sample);
-    
+    console.log(timeConverter(sample.timestamp));
     
     //for (let i = 0; i < ourBoard.numberOfChannels(); i++) {
             //console.log(`Channel ${(i + 1)}: ${sample.channelData[i].toFixed(8)} Volts.`);
@@ -19,7 +18,22 @@ module.exports = {
     }
  }
 
-       
+ 
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+    var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();        
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+}
+  
+  
       
         
   
