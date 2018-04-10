5bacdbbb04f6eccfec0d26204782cfd05d48efe6
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-04-10 13:57:14 +0200

    cleanup openData.js

--- src/functions/openData.js
+++ src/functions/openData.js
@@ -9,19 +9,10 @@ module.exports = {
 
 const fs = require('fs');
 const filename = "data/data.txt"
-var cells;
-
-
-var str = fs.readFile(filename, 'utf8', function (err, data) {
-    if (err) throw err;
-    console.log('OK: ' + filename);
-})
-
 
 function getFiledata() {
-
     var str = fs.readFileSync(filename, 'utf8');
     var rows = str.split('\n');
-    cells = rows.map(row => row.split(','));
+    var cells = rows.map(row => row.split(','));
     return cells;
 }
