3e195a31a8883aa7b1cfef8d8e4d691336af4efc
Michael Job <budiman.job@gmail.com>
2018-04-23 21:29:52 +0200

    tests for mathFunctions

--- test/testEEGFunctions.js
+++ test/testEEGFunctions.js
@@ -1,12 +1,16 @@
-const eegFunctions = require('./../src/functions/eegFunctions');
-
 // test the eegFunctions
+const eegFunctions = require('./../src/functions/eegFunctions');
 var assert = require('assert');
 
 describe('eegFunctions', function() {
   describe('#subtractBaseline()', function() {
-    it('should 6', function() {
+    it('10 minus avg(2,4,6) should be 6', function() {
       assert.equal(eegFunctions.subtractBaseline([2,4,6], 10), 6);
+    });
+  });
+  describe('#subtractBaselineAllChannels()', function() {
+    it('[10,20] minus avg[(2,4,6),(4,6,8)] should be [6,14]', function() {
+      assert.deepEqual(eegFunctions.subtractBaselineAllChannels([[2,4,6],[4,6,8]], [10,20]), [6,14]);
     });
   });
 });
