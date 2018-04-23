5e01b239f0173361e493d04c67d716d9a4bddb1a
Michael Job <budiman.job@gmail.com>
2018-04-23 14:50:18 +0200

    more generic blink parameters

--- src/blink/blink.js
+++ src/blink/blink.js
@@ -2,6 +2,7 @@
  * extract blinks from eeg signal
  * 
  * first approach: direct volt from channel 2
+ * second approach: channel 1 - 
  * 
  * blinks last from 100 to 400ms in real
  * (EEG data: 20-40Hz)
@@ -14,106 +15,45 @@ module.exports = {
 }
 
 const detectBlink = require('./detectBlink');
-const slots = 10;
-const channel = 0;
+
+const baselineLengthSec = 5;      // time in seconds for baseline
+const slots = 10;                 // data points per slot
+const channel = 1;                // number of channel ( from 1 to 8 )
+const sampleRate = 250;           // 250Hz
+const baselineSlots = baselineLengthSec*sampleRate/slots; // number of slots in baseline (at 250Hz)
+
 
 let averages = [];
+let average;
 let baseline  = [];
 let count = 0;
 let avgInterval = 0;
 
-
-
 function getSampleAverages(sample) {
-
-   // console.log("Channel 1: " + sample.channelData[0].toFixed(8) * 1000000 + " microVolts.");
-    // prints to the console
-    //  "Channel 1: 0.00001987 Volts."
-    //  "Channel 2: 0.00002255 Volts."
-    //  ...
-    //  "Channel 8: -0.00001875 Volts."
 
     baseline = getBaseline();
 
     if (count < slots) {
-        avgInterval = avgInterval+Number(sample.channelData[channel].toFixed(20) * 1000000); //microVolts
+        avgInterval = avgInterval+Number((sample.channelData[channel-1] * 1000000).toFixed(20)); //microVolts
         count++;
     } else if (count === slots) {
         averages.push(avgInterval/slots);
+        average = Number(avgInterval/slots);
         count = 0;
         avgInterval = 0;
 
-        if(baseline.length>=50) {
-            detectBlink.compareAverages(baseline,averages,slots);
+        //if baseline is at least 1250 samples (5 sec.) -> detect Blinks
+        if(baseline.length*slots>=baselineLengthSec*sampleRate) {
+            detectBlink.compareAverages(baseline,average,slots);
         }
     }
 
 }
+
 function getBaseline() {
-    if(averages.length>250){
-        return averages.slice(0,250); //first 10s
+    if(averages.length>baselineSlots){
+        return averages.slice(10,10+baselineSlots); //skip first 10 data points to minimize error data at start
     } else {
         return averages;
     }
 }
-
-//
-// average = 0;
-// count = 0;
-// blinkSampleSize = 25; //sample comes in 250 Hz : 100ms are 25 samples
-// averages = [];
-//
-// // sum up volts from electrode 2 and build average
-// function getAveragesFrom2(sample) {
-//
-//     //multiply volts for easier visual comparison
-//     var volt2 = sample.channelData[1].toFixed(20) * 100000;
-//
-//     if (count < 25) {
-//         average = average+Number(volt2);
-//         count++;
-//     } else if (count === 25) {
-//         average = average / 25;
-//         averages.push(average);
-//         count = 0;
-//         average = 0;
-//         compareAverages(sample);
-//     }
-// }
-//
-// // compare last two averages for blink detection
-// function compareAverages(sample) {
-//     var size = averages.length;
-//     if (size > 7) {
-//         // varianz over 3 values
-//         // var varianz = (Math.abs(averages[size-3])+Math.abs(averages[size-4])+Math.abs(averages[size-5])) / 3;
-//
-//         // varianz over all values
-//         var varianz = averages.reduce(function(sum, a) { return sum + Math.abs(a) }, 0) / (averages.length||1);
-//
-//         // blink as difference of current slot to last slot
-//         var blink = averages[size-1] - averages[size-2];
-//
-// //        console.log("blink   "+blink);
-// //        console.log("varianz "+varianz);
-//         process.stdout.write("listening to EEG " + timeConverter(sample.timestamp) + "\r");
-//         if (blink > varianz) {
-//             console.log("You blinked at "+timeConverter(sample.timestamp));
-//         }
-//     }
-// }
-//
-// // get human readable time
-// function timeConverter(UNIX_timestamp) {
-//     var a = new Date(UNIX_timestamp);
-//     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//     var year = a.getFullYear();
-//     var month = months[a.getMonth()];
-//     var date = a.getDate();
-//     var hour = a.getHours();
-//     var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
-//     var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
-//     var millisec = a.getMilliseconds() < 10 ? '00' + a.getMilliseconds() : a.getMilliseconds() < 100 ? '0' + a.getMilliseconds() : a.getMilliseconds();;
-//     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec + ':' + millisec;
-//     return time; // + " @ " + UNIX_timestamp;
-// }
--- src/blink/detectBlink.js
+++ src/blink/detectBlink.js
@@ -1,42 +1,41 @@
 const mathFunctions = require('../functions/mathFunctions');
+const eegFunctions = require('./../functions/eegFunctions');
 
 module.exports = {
     compareAverages: detectBlink
 };
 
+const subtractBaseline = true;  //subtract baseline from slots
+var showBaseline = true;
 
-function detectBlink(baseline, averages, slots){
+function detectBlink(baseline, average, slots) {
     //deviation factor
-    var a=1.5;
+    var a = 1.5;
     var mean = mathFunctions.getAverage(baseline);
     var standardDeviation = mathFunctions.getStandardDeviation(baseline);
 
-    if(baseline.length==250){
+    if (subtractBaseline) {
+        averages = eegFunctions.subtractBaseline(baseline, average, slots);
+    }  
+
+    if (showBaseline) {
+        console.log("=================Baseline=================");
+        console.log("  Baseline size:\t" + baseline.length);
+        console.log("  Average(mean):\t" + mathFunctions.getAverage(baseline).toFixed(2));
+        console.log("  Variance:\t\t" + mathFunctions.getVariance(baseline).toFixed(2));
+        console.log("  Standard deviation:\t" + mathFunctions.getStandardDeviation(baseline).toFixed(2));
+        console.log("  mean-standardDev*a:\t " + Number(mean - standardDeviation * a).toFixed(2));
+        console.log("  Max Value:\t\t" + mathFunctions.getMaxValue(baseline).toFixed(2));
+        console.log("  Min Value:\t\t" + mathFunctions.getMinValue(baseline).toFixed(2));
         console.log("==============================================");
-        console.log("  Average size:\t\t"+averages.length);
-        console.log("  Baseline size:\t"+baseline.length);
-        console.log("  Average(mean):\t"+mathFunctions.getAverage(baseline).toFixed(2));
-        console.log("  Variance:\t\t"+mathFunctions.getVariance(baseline).toFixed(2));
-        console.log("  Standard deviation:\t"+mathFunctions.getStandardDeviation(baseline).toFixed(2));
-        console.log("  mean-standardDev*a:\t "+Number(mean-standardDeviation*a).toFixed(2));
-        console.log("  Max Value:\t\t"+mathFunctions.getMaxValue(baseline).toFixed(2));
-        console.log("  Min Value:\t\t"+mathFunctions.getMinValue(baseline).toFixed(2));
-        console.log("==============================================");
+        showBaseline = false;
     }
 
-    for (var i=0; i<averages.length; i++) {
-            //if current value is bigger then  mean - standardDeviation * a  it is a blink
-            if(Number(mean-standardDeviation*a) > averages[i]){
-                console.log("BLINK: Row: "+i*slots+"-"+(i*slots+slots)+"\t diff: "+mathFunctions.percentageChange(averages[i],mean).toFixed(2)+"%\t value: "+Number(averages[i]).toFixed(2));
-
-                //skip next 25 slots (1 second)
-                if(i+25<averages.length){
-                    i=i+25;
-                } else {
-                    i = averages.length;
-                }
-            }
-    }
+        //if current value is bigger then  mean - standardDeviation * a  it is a blink
+        if (Number(mean - standardDeviation * a) > average) {
+            console.log("BLINK: \t average: "+average+"\t at "+new Date());
+        }
+   
 }
 
 
