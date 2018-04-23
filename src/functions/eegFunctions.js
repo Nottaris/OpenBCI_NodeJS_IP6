776f978e9544f43bd04ce7cabe13166f5d3cb702
Michael Job <budiman.job@gmail.com>
2018-04-23 14:50:31 +0200

    draft of eegFunctions

---
+++ src/functions/eegFunctions.js
@@ -0,0 +1,15 @@
+module.exports = {
+    subtractBaseline
+}
+
+// returns slot minus baseline averages
+function subtractBaseline(baseline, averages, slots){
+
+    let averagesRaw = averages;
+    let baselineRaw = baseline;
+    let averagesCor = averages;
+
+
+
+    return averagesCor;
+}
