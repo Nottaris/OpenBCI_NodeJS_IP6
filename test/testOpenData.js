0c025e03bbb1c1d50ae0c3ea991e700310afc17c
Michael Job <budiman.job@gmail.com>
2018-04-24 09:09:58 +0200

    testOpenData

---
+++ test/testOpenData.js
@@ -0,0 +1,38 @@
+// test the openData
+const openData = require('./../src/functions/openData');
+var assert = require('assert');
+const fs = require('fs');
+const testfilepath = "data/testdata";
+
+describe('openData', function () {
+  before(function () {
+    var stream = fs.createWriteStream(testfilepath+'.txt', { flags: 'a' });
+    stream.write("123");
+    stream.close;
+  });
+  describe('#getFiledata()', function () {
+    it('data/testdata.txt should be "123"', function () {
+      assert.equal(openData.getFiledata(testfilepath+'.txt'), "123");
+    });
+  });
+  after(function () {
+    fs.unlink(testfilepath+'.txt', (err) => {
+      if (err) throw err;
+    });
+  });
+  before(function () {
+    var stream = fs.createWriteStream(testfilepath+'.json', { flags: 'a' });
+    stream.write(JSON.stringify({key:123}));
+    stream.close;
+  });
+  describe('#loadJSON()', function () {
+    it('data/testdata.json should be {key:123}', function () {
+      assert.equal(openData.getFiledata(testfilepath+'.json'), '{"key":123}');
+    });
+  });
+  after(function () {
+    fs.unlink(testfilepath+'.json', (err) => {
+      if (err) throw err;
+    });
+  });
+});
