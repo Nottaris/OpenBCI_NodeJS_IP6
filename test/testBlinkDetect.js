0894abd7b3f60cfe48d6c38fe0c650a1402852f6
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-05-01 12:27:30 +0200

    Update blink detect

--- test/testBlinkDetect.js
+++ test/testBlinkDetect.js
@@ -1,22 +1,24 @@
-// // test the openData
-// const openData = require('./../src/blink/blink');
-//
-// describe('blink', function () {
-//   before(function () {
-//       data = openData.loadJSON("test/data/data-2018-5-1-11-23-10-TESTDATA-5-BLINKS.json");
-//   });
-//   describe('detectBlinks', function () {
-//     it('5 blinks should be detected', function () {
-//
-//       data.forEach(function(sample) {
-//           blink.getBlinks(sample);
-//       });
-//
-//       assert.equal(blink.getBlinkcount(), 5);
-//     });
-//
-//   });
-//   after(function () {
-//     data = null;
-//   });
-// });
+// test the openData
+const blink = require('./../src/blink/blink');
+const openData = require('./../src/functions/openData');
+var assert = require('assert');
+
+describe('blink', function () {
+  before(function () {
+      data = openData.loadJSON("../../test/data/data-2018-5-1-11-23-10-TESTDATA-5-BLINKS.json");
+  });
+  describe('detectBlinks', function () {
+    it('5 blinks should be detected', function () {
+
+      data.forEach(function(sample) {
+          blink.getBlinks(sample);
+      });
+
+      assert.equal(blink.getBlinkcount(), 5);
+    });
+
+  });
+  after(function () {
+    data = null;
+  });
+});
