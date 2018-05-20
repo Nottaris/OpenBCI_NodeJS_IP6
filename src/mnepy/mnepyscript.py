3e7189c553f34e796627c9a5e3db8c101b820eb8
Michael Job <budiman.job@gmail.com>
2018-05-20 15:25:06 +0200

    got rid of first example

--- src/mnepy/mnepyscript.py
+++ src/mnepy/mnepyscript.py
@@ -1,27 +1,8 @@
 import mne
 import numpy as np
 
-# Generate some random data
-data = np.random.randn(8, 1000)
-
-# Initialize an info structure
-info = mne.create_info(
-    ch_names=['EEG1', 'EEG2', 'EEG3', 'EEG4', 'EEG5', 'EEG6', 'EEG7', 'EEG8'],
-    ch_types=['eeg', 'eeg', 'eeg', 'eeg', 'eeg', 'eeg', 'eeg', 'eeg'],
-    sfreq=250
-)
-
-custom_raw = mne.io.RawArray(data, info)
-print(custom_raw)
-
-custom_raw.plot(block=True, lowpass=40)
-
-
-
-
-
 # Generate some random data: 10 epochs, 8 channels, 2 seconds per epoch
-sfreq = 100
+sfreq = 250
 data = np.random.randn(10, 8, sfreq * 2)
 
 # Initialize an info structure
