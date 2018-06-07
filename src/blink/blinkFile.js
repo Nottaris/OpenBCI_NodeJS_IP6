67cba560f84e8952673558b10556935f0e8d1c3f
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-06-07 20:10:14 +0200

    plot data

--- src/blink/blinkFile.js
+++ src/blink/blinkFile.js
@@ -1,57 +1,30 @@
 const openData = require('./../functions/openData');
+//const blink = require('./../blink/blink');
+
 var PythonShell = require('python-shell');
-
-
 
 
 console.log("blinkfile");
 
- var data = openData.loadJSON("../../test/data/data-2018-5-1-11-23-10-TESTDATA-5-BLINKS.json");
-
-
+var data = openData.loadJSON("../../test/data/data-2018-5-1-11-23-10-TESTDATA-5-BLINKS.json");
 var pyshell = new PythonShell('./../pyscripts/butterworthBandpass.py');
 
-pyshell.stdout.on('data', function (data) {
-    console.log(data);
+// received a message sent from the Python script (a simple "print" statement)
+pyshell.stdout.on('data', function (value) {
+     console.log(value);
+     //TODO: send filtered value to detectBlink
+     //blink.getBlinks(value);
 });
 
-
-
+// sends channel data to the Python script via stdin
 data.forEach(function(sample) {
      if(sample.channelData[0]!==0) {
-        // sends a message to the Python script via stdin
-        pyshell.send(sample.channelData[0] * 1000000);
+        pyshell.send((sample.channelData[0]* 1000000).toFixed(20));
      }
 })
 
-
 // end the input stream and allow the process to exit
-pyshell.end(function (err,code,signal) {
+pyshell.end(function (err) {
   if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
-  console.log('finished');
-});
-
-
-var pyshell = new PythonShell('./../pyscripts/butterworthBandpass.py');
-
-pyshell.stdout.on('data', function (data) {
-    console.log(data);
-});
-
-
-
-data[1].channelData[0]
-
-
-
-// end the input stream and allow the process to exit
-pyshell.end(function (err,code,signal) {
-  if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
   console.log('finished');
 });
--- src/pyscripts/butterworthBandpass.py
+++ src/pyscripts/butterworthBandpass.py
@@ -1,15 +1,8 @@
 from scipy.signal import butter, lfilter
-import sys, json, numpy as np
+import sys, numpy as np, matplotlib.pyplot as plt
 
 
-# Source http://scipy-cookbook.readthedocs.io/items/ButterworthBandpass.html
-#Read data from stdin
-def read_in():
-    lines = sys.stdin.readlines()
-    for line in sys.stdin:
-        print(line[:-1])
-    # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+# Source butter_bandpass http://scipy-cookbook.readthedocs.io/items/ButterworthBandpass.html
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
@@ -24,55 +17,11 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-
-def mainTEST():
-    # get our data as an array from read_in()
-    lines = read_in()
-
-    # create a numpy array
-    data = np.array(lines)
-
-
-    print(data[0])
-    print(data[1])
-    print(data[2])
-    print(data[3])
-    print(data[4])
-    print(data[5])
-    print(data[6])
-    print(data[7])
-    print(data[8])
-
+def main():
     # Sample rate and desired cutoff frequencies (in Hz).
     fs = 250.0
     lowcut = 5.0
     highcut = 50.0
-
-    y = butter_bandpass_filter(data, lowcut, highcut, fs, order=6)
-    print(y[0])
-    print(y[1])
-    print(y[2])
-    print(y[3])
-    print(y[4])
-    print(y[5])
-    print(y[6])
-    print(y[7])
-    print(y[8])
-
-    #run(np_lines)
-
-    #use numpys sum method to find sum of all elements in the array
-    lines_sum = np.sum(data)
-
-    #return the sum to the output stream
-    print(lines_sum)
-
-
-def main():
-    # Sample rate and desired cutoff frequencies (in Hz).
-    fs = 250.0
-    lowcut = 1.0
-    highcut = 20.0
 
     # get our data as an array from read_in()
     lines = []
@@ -88,6 +37,28 @@ def main():
     # send filterd data back to node
     for f in filterdData:
         print(f)
+
+    # Plot original and filtered data
+    plt.figure(1)
+    plt.subplot(211)
+    plt.title('Eye Blink - No Filter')
+    plt.plot(data, label="Original Data")
+    plt.ylabel('microVolts')
+    plt.xlabel('Samples')
+    plt.legend(loc='upper right')
+    plt.grid(True)
+
+    plt.subplot(212)
+    plt.title('Eye Blink - Bandpass Filter %d - %d Hz' % (lowcut, highcut))
+    plt.plot(filterdData, label="Filterd Data", color='r')
+    plt.ylabel('microVolts')
+    plt.xlabel('Samples')
+    plt.grid(True)
+    plt.legend(loc='upper right')
+    axes = plt.gca()
+    axes.set_ylim([-4000, 4000])
+
+    plt.show()
 
 
 #start process
