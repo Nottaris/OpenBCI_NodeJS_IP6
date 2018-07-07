03dc34f42a0ac668ec7080f704d37b982ee18ff5
Michael Job <budiman.job@gmail.com>
2018-07-07 19:33:35 +0200

    time sync only if not simulate

--- src/board/openBoard.js
+++ src/board/openBoard.js
@@ -16,72 +16,74 @@ module.exports = {
     start
 };
 
-function start(sampleFunction,boardSettings){
+function start(sampleFunction, boardSettings) {
     const debug = boardSettings.debug; // Pretty print any bytes in and out... it's amazing...
     const verbose = boardSettings.verbose; // Adds verbosity to functions
+    const simulate = boardSettings.simulate;
     const resyncPeriodMin = 5; // re sync every five minutes
     const secondsInMinute = 60;
     let sampleRate = 250;
     const Cyton = require('openbci-cyton');
     let ourBoard = new Cyton({
-        simulate: boardSettings.simulate,
+        simulate: simulate,
         debug: debug,
-        verbose:  verbose
+        verbose: verbose
     });
 
 
     ourBoard.autoFindOpenBCIBoard()
         .then(portName => {
-                if (portName) {
-                    connectToBoard(portName);
-                } else {
-                    /** Unable to auto find OpenBCI board */
-                    console.log('Unable to auto find OpenBCI board');
-                }
-            })
+            if (portName) {
+                connectToBoard(portName);
+            } else {
+                /** Unable to auto find OpenBCI board */
+                console.log('Unable to auto find OpenBCI board');
+            }
+        })
         .catch((err) => {
             //Workaraound: If autofind board doesn't work(windows 10) try it with COM13
             var portName = "COM13";
             connectToBoard(portName);
         });
 
-        function connectToBoard(portName){
-            /**
-             * Connect to the board with portName
-             * Only works if one board is plugged in
-             * i.e. ourBoard.connect(portName).....
-             */
+    function connectToBoard(portName) {
+        /**
+         * Connect to the board with portName
+         * Only works if one board is plugged in
+         * i.e. ourBoard.connect(portName).....
+         */
 
-            ourBoard.connect(portName) // Port name is a serial port name, see `.listPorts()`
-                .then(() => {
-                    ourBoard.syncRegisterSettings()
-                        .then((cs) => {
-                            return ourBoard.streamStart();
-                        })
-                        .catch((err) => {
-                            return ourBoard.streamStart();
-                        })
-                        .catch((err) => {
-                            console.log('fatal err', err);
-                            process.exit(0);
-                        });
-
-                    //log firmware version
-                    if(debug){
-                        console.log("Firmware:");
-                        console.log("== v2: " + ourBoard.usingVersionTwoFirmware());
-                        console.log("== v3: " + ourBoard.usingVersionThreeFirmware());
-                        console.log(">= v2: " + ourBoard.usingAtLeastVersionTwoFirmware());
-                    }
-
-                    //.channelSet(channelNumber,powerDown,gain,inputType,bias,srb2,srb1)
-                    // deactivate unused channels and activate bias and srb2
-                    boardSettings.channelsOff.forEach(function(channelState,index){
-                        ourBoard.channelSet(index+1,channelState,24,'normal',true,true,false);
+        ourBoard.connect(portName) // Port name is a serial port name, see `.listPorts()`
+            .then(() => {
+                ourBoard.syncRegisterSettings()
+                    .then((cs) => {
+                        return ourBoard.streamStart();
+                    })
+                    .catch((err) => {
+                        return ourBoard.streamStart();
+                    })
+                    .catch((err) => {
+                        console.log('fatal err', err);
+                        process.exit(0);
                     });
 
-                    ourBoard.on('sample', (sample) => {
-                         // Resynchronize every every 5 minutes
+                //log firmware version
+                if (debug) {
+                    console.log("Firmware:");
+                    console.log("== v2: " + ourBoard.usingVersionTwoFirmware());
+                    console.log("== v3: " + ourBoard.usingVersionThreeFirmware());
+                    console.log(">= v2: " + ourBoard.usingAtLeastVersionTwoFirmware());
+                }
+
+                //.channelSet(channelNumber,powerDown,gain,inputType,bias,srb2,srb1)
+                // deactivate unused channels and activate bias and srb2
+                boardSettings.channelsOff.forEach(function (channelState, index) {
+                    ourBoard.channelSet(index + 1, channelState, 24, 'normal', true, true, false);
+                });
+
+                ourBoard.on('sample', (sample) => {
+                    // Resynchronize every every 5 minutes (only if simulate === false)
+                    if (!simulate) {
                         if (sample._count % (sampleRate * resyncPeriodMin * secondsInMinute) === 0) {
                             ourBoard.syncClocksFull()
                                 .then(syncObj => {
@@ -97,17 +99,18 @@ function start(sampleFunction,boardSettings){
                                     }
                                 });
                         }
-                        sampleFunction(sample);
-                    });
+                    }
+                    sampleFunction(sample);
                 });
-        }
+            });
+    }
 
     function exitHandler(options, err) {
         if (options.cleanup) {
             if (verbose) console.log('clean');
             ourBoard.removeAllListeners();
             /** Do additional clean up here */
-            if(boardSettings.control === 'save'){
+            if (boardSettings.control === 'save') {
                 console.log("fix is called");
                 saveData.fixJsonFile();
             }
