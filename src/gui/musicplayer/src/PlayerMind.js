db03211ae5302982a133d267bb908504c6dfe75c
Michael Job <michael.job@students.fhnw.ch>
2018-07-27 10:06:30 +0200

    training texts enhanced

--- src/gui/musicplayer/src/PlayerMind.js
+++ src/gui/musicplayer/src/PlayerMind.js
@@ -19,13 +19,19 @@ export default class PlayerBlink extends React.Component {
             audioVolume: 0.5,
             trackNr: 0,
             currentCmd: 'no',
-            commands: {
-                'volup': 'Focus on volume up and think of screaming with your mouth wide open.',
-                'playpause': 'Focus on playing and think of leaning and going forward.',
-                'next': 'Focus on next and think of open and closing your right hand to a fist.',
-                'prev': 'Focus on prev and think of open and closing your left hand to a fist.',
-                'voldown': 'Focus on volume down and think of lowering you head.'
-            }
+            commands: ['volup', 'playpause', 'next', 'prev', 'voldown'],
+            commandinfos: [' volume up - open mouth',
+                'play - go forward',
+                'next - right hand',
+                'prev - left hand',
+                'volume down - move feet'
+            ],
+            commandpreinfos: ['Focus on volume up and think of screaming with your mouth wide open.',
+                'Focus on playing and think of leaning and going forward.',
+                'Focus on next and think of open and closing your right hand to a fist.',
+                'Focus on prev and think of open and closing your left hand to a fist.',
+                'Focus on volume down and think of moving your feet.'
+            ]
         };
 
         this.clickCommand = this.clickCommand.bind(this);
@@ -60,17 +66,21 @@ export default class PlayerBlink extends React.Component {
         let trainIcon = document.getElementById('training').getElementsByClassName('fa')[0];
         trainIcon.style.color = "lightblue";
         let infotext = document.getElementById('infotext');
+        //init text
         infotext.innerText = "Training will start soon. Relax and sit comfy.";
+        //info for first command training
+        setTimeout(function () {
+            infotext.innerText = "Focus on volume up and think of screaming with your mouth wide open.";
+        }, 2000);
 
         //train each command
         let i = 0;
-        const commands = Object.keys(this.state.commands);
         let interval = setInterval(function () {
             if (i === 5) {
                 this.trainingStartML();
                 clearInterval(interval);
             } else {
-                this.showtrainCommand(commands[i]);
+                this.showtrainCommand(this.state.commands[i]);
             }
             i++;
         }.bind(this), this.state.trainingTime + 6000);  //training time plus pause
@@ -80,7 +90,7 @@ export default class PlayerBlink extends React.Component {
     trainingStartML() {
         let infotext = document.getElementById('infotext');
         infotext.innerText = "Please wait. Training data is processed.";
-        sendTrainingCmd({command:'init', slots:0});
+        sendTrainingCmd({command: 'init', slots: 0});
         setTimeout(function () {
             this.trainingFinished();
         }.bind(this), 6000);
@@ -98,13 +108,16 @@ export default class PlayerBlink extends React.Component {
     }
 
     //show training pause
-    trainingPause() {
+    trainingPause(info) {
         let cmdIcons = document.getElementsByClassName('cmd');
         for (var i = 0; i < cmdIcons.length; i++) {
             cmdIcons[i].style.color = "#1c456e";
         }
         let infotext = document.getElementById('infotext');
         infotext.innerText = "...relax - stay calm...";
+        setTimeout(function () {
+            infotext.innerText = info;
+        }, 3000);
     }
 
     toggleButtonsOnTraining(disable) {
@@ -124,7 +137,8 @@ export default class PlayerBlink extends React.Component {
     showtrainCommand(command) {
         //show info and start highligthing command to train
         let infotext = document.getElementById('infotext');
-        infotext.innerText = this.state.commands[command];
+        let cmdindex = this.state.commands.indexOf(command);
+        infotext.innerText = this.state.commandinfos[cmdindex];
         //reset all icons to default color
         let cmdIcons = document.getElementsByClassName('cmd');
         for (var i = 0; i < cmdIcons.length; i++) {
@@ -157,9 +171,16 @@ export default class PlayerBlink extends React.Component {
 
     //training of command x (init on server)
     trainCommand(command) {
-        let trainingSlotSize = this.state.trainingTime/4;    //on 250 Hz, each 4 ms a sample is recorded
-        sendTrainingCmd({command:command, slots:trainingSlotSize});    //after training phase - server saves past data to file
-        this.trainingPause(); //gui: show training pause
+        let trainingSlotSize = this.state.trainingTime / 4;    //on 250 Hz, each 4 ms a sample is recorded
+        sendTrainingCmd({command: command, slots: trainingSlotSize});    //after training phase - server saves past data to file
+        // get next command
+        let cmdindex = this.state.commands.indexOf(command);
+        let nextcmd = cmdindex+1;
+        let info = '...relax - stay calm...';
+        if (nextcmd !== 5){
+            info = this.state.commandpreinfos[nextcmd];
+        }
+        this.trainingPause(info); //gui: show training pause
     }
 
 
@@ -200,9 +221,9 @@ export default class PlayerBlink extends React.Component {
                 this.voldown(audio);
                 break;
             case "playpause":
-                if (this.state.playpauseToggle == 'play') {
+                if (this.state.playpauseToggle === 'play') {
                     this.play(audio);
-                } else if (this.state.playpauseToggle == 'pause') {
+                } else if (this.state.playpauseToggle === 'pause') {
                     this.pause(audio);
                 }
                 break;
