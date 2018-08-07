25b019020ae86b6f5bb22415b696c1a65285770e
Michael Job <michael.job@students.fhnw.ch>
2018-08-07 19:38:44 +0200

    test txt with csv data

--- test/testOpenData.js
+++ test/testOpenData.js
@@ -1,5 +1,6 @@
 // test the openData
 const openData = require('./../src/functions/openData');
+const openData2 = require('./../src/functions/openData');
 const assert = require('assert');
 const fs = require('fs');
 const testfilepath = "test/data/";
@@ -8,20 +9,23 @@ describe('openData', function () {
 
 
     before(function () {
-        var streamTxt = fs.createWriteStream(testfilepath + 'OpenDataTestTxT.txt', {flags: 'a'});
-        streamTxt.write("123");
-        streamTxt.close;
 
         var streamJson = fs.createWriteStream(testfilepath + 'OpenDataTestJson.json', {flags: 'a'});
         streamJson.write(JSON.stringify({key: 123}));
         streamJson.close;
 
+        var streamTxt = fs.createWriteStream(testfilepath + 'OpenDataTestTxT.txt', {flags: 'a'});
+        streamTxt.write("123,456,789");
+        streamTxt.close;
+
     });
+
+    this.timeout(15000);
 
 
     describe('#getFiledata()', function () {
-        it('test/data/OpenDataTestTxT.txt should be "123"', function () {
-            assert.equal(openData.getFiledata(testfilepath + 'OpenDataTestTxT.txt'), "123");
+        it('test/data/OpenDataTestTxT.txt should be "\'123\',\'456\',\'789\'"', function () {
+            assert.deepStrictEqual(openData2.getFiledata(testfilepath + 'OpenDataTestTxT.txt'), [['123', '456', '789']]);
         });
     });
 
@@ -33,11 +37,14 @@ describe('openData', function () {
 
 
     after(function () {
-        fs.unlink(testfilepath + 'OpenDataTestTxT.txt', (err) => {
+
+        this.timeout(15000);
+
+        fs.unlink(testfilepath + 'OpenDataTestJson.json', (err) => {
             if (err) throw err;
         });
 
-        fs.unlink(testfilepath + 'OpenDataTestJson.json', (err) => {
+        fs.unlink(testfilepath + 'OpenDataTestTxT.txt', (err) => {
             if (err) throw err;
         });
 
