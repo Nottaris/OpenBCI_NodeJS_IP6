057c9ab218a826e3c352b37fc9ff1d173e404af5
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-08-14 09:45:11 +0200

    Cleanup Player

--- src/gui/musicplayer/src/App.js
+++ src/gui/musicplayer/src/App.js
@@ -38,8 +38,6 @@ class App extends Component {
     }
 
     render() {
-
-
         return (
             <div className={"App "+this.state.selectedPlayer}>
                 <header className="App-header">
--- src/gui/musicplayer/src/components/ControlsP300.js
+++ src/gui/musicplayer/src/components/ControlsP300.js
@@ -39,25 +39,6 @@ export default class Controls extends React.Component {
                     <div id="noCmd5" className="Button"></div>
                 </div>
             </div>
-            //   <div className="Controls">
-            //     <div className="row">
-            //         <div onClick={() => this.setCommand('voldown')} id="voldown" className="Button">
-            //             <i className='P300 fa fa-fw fa-volume-down'></i>
-            //         </div>
-            //         <div onClick={() => this.setCommand('prev')} id="prev" className="Button">
-            //             <i className='P300 fa fa-fw fa-backward'></i>
-            //         </div>
-            //         <div onClick={() => this.setCommand('playpause')} id="playpause" className="Button">
-            //             <i className={"P300 fa fa-fw fa-" + this.props.playpauseToggle}></i>
-            //         </div>
-            //         <div onClick={() => this.setCommand('next')} id="next" className="Button">
-            //             <i className='P300 fa fa-fw fa-forward'></i>
-            //         </div>
-            //         <div onClick={() => this.setCommand('volup')} id="volup" className="Button">
-            //             <i className='P300 fa fa-fw fa-volume-up'></i>
-            //         </div>
-            //     </div>
-            // </div>
         )
     }
 }
