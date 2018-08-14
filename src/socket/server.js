9b217c4db3cca6e08ab4f46335d6c3aa331aef12
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-08-14 12:51:57 +0200

    Cleanup socket server

--- src/socket/server.js
+++ src/socket/server.js
@@ -7,52 +7,46 @@ module.exports = {
     streamData,
     startSocketServer,
     closeSocketServer,
-    subscribeToCmds,
+    subscribeToP300Cmds,
     subscribeToTrainingCmds
-}
+};
 
-const http = require('http');
+const http = require("http");
 const port = 3001;
 let io;
 let app;
 
 // create socket server on port 3001
 function startSocketServer() {
-    app = http.createServer(function (req, res) {
-    });
-    io = require('socket.io').listen(app);
-    io.set('origins', 'http://localhost:*');
+    app = http.createServer();
+    io = require("socket.io").listen(app);
+    io.set("origins", "http://localhost:*");
 
     app.listen(port, function () {
-        console.log('listening on *:' + port);
+        console.log("listening on *:" + port);
     });
 
-    app.on('error', function (e) {
+    app.on("error", function (e) {
         console.log("error " + e);
-        callback(true);
     });
-    io.on('P300command', function (data) {
-        console.log("P300command" + data);
-    });
-
 }
 
 
-function subscribeToCmds(callbackP300commandCmd) {
-    io.on('connection', function (socket) {
-        socket.on('P300command', P300command => callbackP300commandCmd(P300command));
+function subscribeToP300Cmds(callbackP300commandCmd) {
+    io.on("connection", function (socket) {
+        socket.on("P300command", (P300command) => callbackP300commandCmd(P300command));
     });
 }
 
 
 function subscribeToTrainingCmds(callbackTrainingCmd) {
-    io.on('connection', function (socket) {
-        socket.on('training', trainingCmd => callbackTrainingCmd(trainingCmd));
+    io.on("connection", function (socket) {
+        socket.on("training", (trainingCmd) => callbackTrainingCmd(trainingCmd));
     });
 }
 
 function closeSocketServer() {
-    if(app != undefined){
+    if(app !== undefined){
         app.close();
     }
 }
@@ -60,23 +54,23 @@ function closeSocketServer() {
 
 function sendCmd(command) {
     //emmit command event for each
-    io.emit('command', {command: command});
+    io.emit("command", {command: command});
     process.stdout.write("sending commands...\r");
 }
 
 function doCmd(docommand) {
     //emmit command event to execute after its detection
-    io.emit('docommand', {docommand: docommand});
+    io.emit("docommand", {docommand: docommand});
     console.log("sent docommand: " + docommand);
 }
 
 function doBlinkCmd(docommand) {
     //emmit command event to execute after blink detection
-    io.emit('blinkcommand',  {docommand: docommand});
+    io.emit("blinkcommand",  {docommand: docommand});
     console.log("sent blinkcommand");
 }
 
 function streamData(sample) {
     //stream bci data to client
-    io.emit('sample', {sample: sample});
+    io.emit("sample", {sample: sample});
 }
