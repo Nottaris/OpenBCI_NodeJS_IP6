d9aca5147f54e4a88362d86a59c7f5021850329d
Ina Widmer <ina.widmer@students.fhnw.ch>
2018-08-14 12:40:33 +0200

    Cleanup P300

--- src/p300/app.js
+++ src/p300/app.js
@@ -3,15 +3,15 @@
  *
  */
 
-const p300 = require('./p300');
-const openBoard = require('./../board/openBoard');
+const p300 = require("./p300");
+const openBoard = require("./../board/openBoard");
 
 let sampleFunction = p300.getP300;
 
 const boardSettings = {
     verbose: true,                                              //  Print out useful debugging events
     debug: false,                                               //  Print out a raw dump of bytes sent and received
-    simulate: false,                                             // Full functionality, just mock data. Must attach Daisy module by setting
+    simulate: true,                                             // Full functionality, just mock data. Must attach Daisy module by setting
     channelsOff: [false, false, false, false, false, false, false, false],    // power down unused channel 1 - 8
     port: "COM13",                                               // COM Port OpenBCI dongle
     control: "p300"                                             // Control type
